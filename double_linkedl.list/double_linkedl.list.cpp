60a203cc9d85108b4a040068369698c3e0f20a22
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-26 20:36:34 +0700

    prosedur menghapus data

--- double_linkedl.list/double_linkedl.list.cpp
+++ double_linkedl.list/double_linkedl.list.cpp
@@ -137,6 +137,19 @@ void DoubleLinkedList::revtraverse() {
 		}
 	}
 }
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nList is Empty" << endl;
+	}
 
+	cout << "\nEnter the roll number of the student whose record is to be deleted:";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << " deleted " << endl;
+}
 
 	
