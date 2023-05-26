089f6c262ed7ba7240975736018afc9ed751b2d3
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-26 19:14:15 +0700

    prosedur void doublelinkedlist menambahkan node

--- double_linkedl.list/double_linkedl.list.cpp
+++ double_linkedl.list/double_linkedl.list.cpp
@@ -6,7 +6,7 @@ class Node {
 public:
 	int noMhs;
 	string  name;
-	Node* name;
+	Node* next;
 	Node* prev;
 
 };
@@ -23,3 +23,30 @@ public:
 	void hapus();
 	void searchData();
 };
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\mEnter the roll nummber of the student: ";
+	cin >> nim;
+	cout << " \nEnter name of the student: ";
+	cin >> nm;
+
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; //step 2
+	newNode->name = nm; //step 2
+
+	/*insert a node  in the beginning of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) { //Check if data null
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START; //Step 3
+		if (START != NULL)
+			START->prev = newNode; //Step 4
+		newNode->prev = NULL; //Step 5
+		START = newNode; //Step 6
+		return;
+	
+}
