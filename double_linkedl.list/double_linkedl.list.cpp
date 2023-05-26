448f7942891271ffc7e7c6b33d9e208fbe6dfae9
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-26 20:40:25 +0700

    prosedur mencari data

--- double_linkedl.list/double_linkedl.list.cpp
+++ double_linkedl.list/double_linkedl.list.cpp
@@ -151,5 +151,24 @@ void DoubleLinkedList::hapus() {
 	else
 		cout << "Record with roll number " << rollNo << " deleted " << endl;
 }
+void DoubleLinkedList::searchData() {
+	if (listEmpty()) {
+		cout << "\nList is Empty" << endl;
+	}
+
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
+}
+
 
 	
